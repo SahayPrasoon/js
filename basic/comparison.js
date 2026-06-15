061df2b39ad0c00e6c8332ea9c83fee70439e8a8
SahayPrasoon <98304393+SahayPrasoon@users.noreply.github.com>
2026-06-15 05:33:03 +0000

    comparison

---
+++ basic/comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+
+// console.log("2">1); //js converted string to num 
+// console.log("02">1);
+
+console.log(null >0);
+console.log(null >=0);
+console.log(null == 0);
+
+// strict check using "===" checks value and data types of the value 
+
+console.log("2" === 2);
+
+
+
