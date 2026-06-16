b0088644729fc094653096262afb9d302ac03385
SahayPrasoon <98304393+SahayPrasoon@users.noreply.github.com>
2026-06-16 11:27:36 +0000

    strings

---
+++ basic/string.js
@@ -0,0 +1,30 @@
+const name = "prasoon"
+const rep = "30"
+
+// console.log(name + rep);
+
+// console.log(`hello my name is ${name} and age is ${rep}`);
+
+const nameme = new String ("xenmode")
+// console.log(nameme[0]);
+// console.log(nameme.__proto__);
+
+// console.log(nameme.length)
+// console.log(nameme.toUpperCase())
+
+
+// console.log(nameme.charAt(2));
+// console.log(nameme.indexOf('e'));
+
+// console.log(nameme.substring(0,4));
+const anotherString = nameme.slice(-7,6)
+console.log(anotherString);
+
+
+const namememe = "    prasoon     "
+// console.log(namememe);
+
+console.log(namememe.trim())
+console.log(namememe.replace('o','u'))
+
+
