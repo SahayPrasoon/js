ee6a904ad6ebc27354d4c8bf7316103a03eb1f9e
SahayPrasoon <98304393+SahayPrasoon@users.noreply.github.com>
2026-06-16 09:51:39 +0000

    dataType & stack and heap

---
+++ basic/datatypesSummary.js
@@ -0,0 +1,47 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// stack(non primitive) and heap(primitive)
+let name ="naam"
+let anotherName = name;
+
+anotherName ="sam"
+
+console.log(name);
+
+console.log(anotherName);
+
+// console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
