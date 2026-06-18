bbceb3981d74b9ff42206f9de089f376818d2dd2
SahayPrasoon <98304393+SahayPrasoon@users.noreply.github.com>
2026-06-18 11:08:53 +0000

    array

---
+++ basic2/array.js
@@ -0,0 +1,31 @@
+const myArr = [1,2,4,5,6,7]
+// ways to declare arr
+
+const mrarr = new Array(1,2,3,4);
+const hero = ["kuch", "bhi","yaar"]
+
+//methods in array
+// myArr.push(3);
+// myArr.pop()
+
+// myArr.unshift(2);
+// myArr.shift();
+ 
+// console.log(myArr.includes(8)) // checks and answers the finding in boolean value
+
+
+// const newarr = myArr.join()
+
+// console.log(typeof newarr)
+// console.log(myArr)
+
+
+// slice and splice methods
+const n1 = myArr.slice(1,3)
+const n2 = myArr.splice(1,3)
+console.log(myArr);
+
+console.log(n1)
+console.log(n2);
+
+
