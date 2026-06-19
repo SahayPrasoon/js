55e2a4e3add35a583e27b1a76ddda206154e584f
SahayPrasoon <98304393+SahayPrasoon@users.noreply.github.com>
2026-06-19 12:33:41 +0000

    functions

---
+++ basic3/functions.js
@@ -0,0 +1,13 @@
+function sayname(){
+    console.log("n");
+    console.log("a");
+    console.log("m");
+    console.log("e");
+}
+// sayname() //we have executed the function
+
+function addnum(num1, num2){
+    console.log(num1+num2)
+}
+
+addnum(2,3)
