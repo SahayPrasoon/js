7ff9556a23eb5a1a384007359260e69a2731bd8a
SahayPrasoon <98304393+SahayPrasoon@users.noreply.github.com>
2026-06-23 06:32:56 +0000

    comm

--- basic3/arrowfun.js
+++ basic3/arrowfun.js
@@ -3,8 +3,7 @@ const name ={
     age: 22,
     adduser: function(){
         console.log(`${this.username}, user added`);
-        console.log(this);  //this tells about current context
-        
+        console.log(this);  //this tells about current context  
     }  
 }
 //  name.adduser()
@@ -13,11 +12,30 @@ const name ={
 
 // console.log(this); // nothing is declared in global variable and due to this its returning empty object
 
-
 // function chaitea(){
 //     let naam= "name"
 //     console.log(this.naam); //this does not work in function
 // }
 // chaitea()
+
+const tea = () =>{
+    let naam ="name"
+    console.log(this.naam);
+    
+}
+// tea()
+
+//ways to declare arrow function 
+
+() =>{}
+// const age = (num1, num2) =>{ // declaring with variable: return is needed explicit return
+//     return num1 + num2
+// }
+
+// const age =(num1, num2)=> (num1 + num2) //implicit declaration it does not require return statement: useful for objects
+const age = (num1, num2) => ({name: "naaaaaam"})
+
+console.log(age(2,3));
+
 
 
