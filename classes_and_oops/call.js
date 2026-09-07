aa0598ab902dcd549586f3b4245a6c38761533fa
SahayPrasoon <98304393+SahayPrasoon@users.noreply.github.com>
2026-09-07 07:12:54 +0000

    call.js completed

--- classes_and_oops/call.js
+++ classes_and_oops/call.js
@@ -1,12 +1,12 @@
 function SetUsername(username){
     //complex DB calls
     this.username = username
-    console.log("called");
+    console.log("called"); // call hoga but usmei context remove hote hi call stack se reference gayab ho jayega 
 }
 
 function createUser(username, email, password){
-    SetUsername.call(this, username)
-   
+    SetUsername.call(this, username) // .call is used in for holding the reference context value of it without it it only calls and removes the context 
+                                    //.call only call and new refernce will be given and need to be given its own "this" current context for it removal ke baad bhi recover rakhta hai 
     this.email = email
     this.password = password
 }
