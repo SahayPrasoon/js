7f0d41ad898d8b6fe577dcf8ad66a071b7ad2ec3
SahayPrasoon <98304393+SahayPrasoon@users.noreply.github.com>
2026-09-07 10:22:43 +0000

    commit getter and setter

--- classes_and_oops/getter_setter.js
+++ classes_and_oops/getter_setter.js
@@ -3,20 +3,20 @@ class User {
         this.email = email;
         this.password = password
     }
-
+//when get & set and constructor method work simultaneously on same property we tend to fill up whole of call stack
     get email(){
-        return this._email.toUpperCase()
+        return this._email.toUpperCase() //get value of email aur touppercase mei change 
     }
     set email(value){
         this._email = value
     }
 
     get password(){
-        return `${this._password}hitesh`
+        return `${this._password}hitesh` //._ko hum set kardete hai this same bacha deta hai override kardeta hai better chalta hai
     }
 
     set password(value){
-        this._password = value
+        this._password = value //set value of password 
     }
 }
 
